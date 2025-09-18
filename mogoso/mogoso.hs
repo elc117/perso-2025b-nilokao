d230771b8e3a50c3ea79dfc06e6429c4696a816a
nicolas <nicolasmlongo@gmail.com>
2025-09-18 00:20:41 -0300

    testes de letras funcionando, teclado adicionado mas nao funcional, trabalho 90% pronto

--- mogoso/mogoso.hs
+++ mogoso/mogoso.hs
@@ -9,6 +9,7 @@ import Data.Text.Lazy (Text)
 import qualified Data.Text as TS
 import qualified Data.Text.IO as TIO
 import qualified Data.Text.Lazy as TL
+import Data.Char (toLower)
 
 -- lê lista de palavras
 myWords :: IO [TL.Text]
@@ -20,6 +21,25 @@ getRandomWord = do
     ws <- myWords
     index <- randomRIO (0, length ws - 1)
     return (ws !! index)
+
+removeOne :: Eq a => a -> [a] -> [a]
+removeOne _ [] = []
+removeOne c (x:xs)
+    | c == x    = xs
+    | otherwise = x : removeOne c xs
+
+testWord :: String -> String -> [Int]
+testWord tentativa word =
+    let greens   = [ if t == p then 2 else -1 | (t,p) <- zip tentativa word ]
+        restWord = [ p | (g,p) <- zip greens word, g /= 2 ]
+    in mark greens tentativa restWord
+
+mark :: [Int] -> String -> String -> [Int]
+mark [] [] _ = []
+mark (g:gs) (t:ts) rest
+    | g == 2          = 2 : mark gs ts rest
+    | t `elem` rest   = 1 : mark gs ts (removeOne t rest)
+    | otherwise       = 0 : mark gs ts rest
 
 main :: IO ()
 main = do
@@ -46,8 +66,16 @@ main = do
         post "/mogoso" $ do
             tentativa <- formParam "tentativa"
             w <- liftIO (readIORef ref)
+            ws <- liftIO myWords
             setHeader "Access-Control-Allow-Origin" "*"
 
-            if TL.toLower tentativa == TL.toLower w
-                then text ("Acertou! A palavra era: " <> w)
-                else text ("Errou! A palavra era: " <> w)
+            let tentativaStr   = TL.unpack (TL.strip tentativa)
+                wordStr        = TL.unpack w
+                tentativaLower = map toLower tentativaStr
+                wsLower        = map (map toLower . TL.unpack) ws
+
+            if tentativaLower `notElem` wsLower
+            then json ("Palavra inválida!" :: String)
+            else do
+                let result = testWord tentativaStr wordStr
+                json result
